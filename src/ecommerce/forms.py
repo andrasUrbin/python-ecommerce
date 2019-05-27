a31ea98818983f1f301c6cc92d08b41f8d2caf4a
andrasUrbin <urbinandras@gmail.com>
2019-05-27 17:14:04 +0200

    registration, username and email check, pwcheck added

--- src/ecommerce/forms.py
+++ src/ecommerce/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth .models import User
 
 
 class ContactForm(forms.Form):
@@ -26,3 +27,44 @@ class ContactForm(forms.Form):
             }
         )
     )
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(
+        widget=forms.PasswordInput()
+    )
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(
+        widget=forms.PasswordInput()
+    )
+    password2 = forms.CharField(
+        label="Confirm password",
+        widget=forms.PasswordInput()
+    )
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        qs = User.objects.filter(username=username)
+        if qs.exists():
+            raise forms.ValidationError("This username is already taken!")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError("This email address is already taken!")
+        return email
+
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("password2")
+        if password2 != password:
+            raise forms.ValidationError("The passwords must match!")
+        return data
--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,13 +1,55 @@
-from django.shortcuts import render
-from .forms import ContactForm
+from django.contrib.auth import authenticate, login, get_user_model
+from django.shortcuts import render, redirect
+from .forms import ContactForm, LoginForm, RegisterForm
 
 
 def home_page(request):
     context = {
         "title":"Home",
-        "content":"Welcome to the home page!"
+        "content":"Welcome to the home page!",
+        "premium_content": "Pretty much premium, right? :D"
+
     }
+
     return render(request, "home_page.html", context)
+
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(request.user.is_authenticated())
+        if user is not None:
+            print(request.user.is_authenticated())
+            login(request, user)
+            return redirect("/")
+        else:
+            print("Smoething went wrong!")
+    return render(request, "auth/login.html", context)
+
+
+User = get_user_model()
+
+
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+    return render(request, "auth/register.html", context)
 
 
 def about_page(request):
